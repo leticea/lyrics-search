bc3a651ec2d47233f80880db7e55037e1f1e11f8
leticea <nascimento.leticeah@gmail.com>
2022-11-12 18:25:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search');
+const songsContainer = document.querySelector('#songs-container');
+const prevAndNextContainer = document.querySelector('#prev-and-next-container');
+
+const apiURL = `https://api.lyrics.ovh`;
+
+form.addEventListener('submit', event => {
+
+    event.preventDefault();
+})
